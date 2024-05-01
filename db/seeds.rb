0f20843274bd1950fc9454bde2c2bd890afb6d5d
Ella Pierre <ellajpierre@gmail.com>
2024-05-01 14:56:42 +0100

    added index, show, new, create action for restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+shack_fuyu = { name: 'Shack-Fuyu', address: '14A Old Compton St, London W1D 4TJ', category: 'japanese' }
+wapalapam = { name: 'Wapalapam', address: 'Le Morne Brabant 91202, Mauritius', category: 'french' }
+balthazar = { name: 'Balthazar', address: '4-6 Russell St, London WC2B 5HZ', category: 'french' }
+duck_rice = { name: 'The Duck and Rice', address: '90 Berwick St, London W1F 0QB', category: 'chinese' }
+
+[pizza_east, shack_fuyu, wapalapam, balthazar, duck_rice].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
