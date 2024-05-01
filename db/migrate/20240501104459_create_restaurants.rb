691b7edb38205f290b14dca541998277c5a602a7
Ella Pierre <ellajpierre@gmail.com>
2024-05-01 12:17:06 +0100

    added review validations

--- db/migrate/20240501104459_create_restaurants.rb
+++ db/migrate/20240501104459_create_restaurants.rb
@@ -1,7 +1,7 @@
 class CreateRestaurants < ActiveRecord::Migration[7.1]
   def change
     create_table :restaurants do |t|
-      t.string :name, presence: true
+      t.string :name
       t.string :address
       t.string :phone_number
       t.string :category
